df8dca35e79abb49099984fdc895ed37b7e07e51
Shawn Weitzel <shawn.weitzel@changepoint.com>
2014-09-23 12:39:21 -0700

    Correct order in assert and add appveyor.yml

--- PseudoLocalizer.Core.Tests/ResxProcessorTests.cs
+++ PseudoLocalizer.Core.Tests/ResxProcessorTests.cs
@@ -26,7 +26,7 @@
                 processor.Transform(inputStream, outputStream);
             }
 
-            Assert.That(File.ReadAllText(Test1FileName), Is.EqualTo(File.ReadAllText(OutputFileName)), "the output file is identical to the input file.");
+            Assert.That(File.ReadAllText(OutputFileName), Is.EqualTo(File.ReadAllText(Test1FileName)), "the output file is identical to the input file.");
         }
 
         [Test]
